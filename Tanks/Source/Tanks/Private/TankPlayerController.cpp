4971bd8cf778cbbac728c95790aecd2f1e19113a
Keiran Jarvis <keiranj777@hotmail.com>
2016-11-25 03:44:58 +0000

    Tank AI controller setup

--- Tanks/Source/Tanks/Private/TankPlayerController.cpp
+++ Tanks/Source/Tanks/Private/TankPlayerController.cpp
@@ -3,6 +3,21 @@
 #include "Tanks.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    auto ControlledTank = GetControlledTank();
+    if(!ControlledTank)
+    {
+         UE_LOG(LogTemp, Warning, TEXT("Player controller not possesing a tank"))
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Player controller possessing: %s"), *(ControlledTank->GetName()))
+    }
+}
+
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
--- Tanks/Source/Tanks/Public/TankPlayerController.h
+++ Tanks/Source/Tanks/Public/TankPlayerController.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #pragma once
 
@@ -16,6 +16,6 @@ class TANKS_API ATankPlayerController : public APlayerController
 	
 public:
     ATank* GetControlledTank() const;
-	
+    void BeginPlay() override;
 	
 };
