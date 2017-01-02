2a6f578b92578b55d936233c2f71ee4115808261
Keiran Jarvis <keiranj777@hotmail.com>
2017-01-02 17:32:22 +0000

    Tank movement

---
+++ Tanks/Source/Tanks/Public/TankTrack.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack is used to apply forces to the tank
+ */
+UCLASS(meta=(BlueprintSpawnableComponent))
+class TANKS_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+    UFUNCTION(BlueprintCallable, Category = Input)
+    void SetThrottle( float Throttle );
+    
+    
+    // Max force per track in Newtons
+    
+    UPROPERTY(EditDefaultsOnly, Category = Input)
+    float TrackMaxDrivingForce = 400000; //assume 40 ton tank and 1 g acceleration
+};
